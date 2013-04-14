495da98474fae6392431bf8bef83450a17a5f1d9
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2013-04-14 00:11:26 -1000

    Random angle and apply height filter. All settings now work as intended

--- structures_mapgen.lua
+++ structures_mapgen.lua
@@ -6,7 +6,7 @@ local MAPGEN_FILE = "mapgen.txt"
 local MAPGEN_PROBABILITY = 0.1
 local MAPGEN_RANGE = 2.5
 local MAPGEN_AVOID_STEPS = 5
-local MAPGEN_FILL = true
+local MAPGEN_FILL = 10
 
 -- Local functions - Table
 
@@ -78,18 +78,26 @@ end
 
 -- Local functions - Spawn
 
-local function spawn_structure (filename, pos, size, distance, node, range, avoid)
+local function spawn_structure (filename, pos, angle, size, distance, range, height, node, avoid)
+	-- randomly choose which direction to avoid in (see below)
+	local avoid_step_x = MAPGEN_AVOID_STEPS
+	if (math.random() < 0.5) then
+		avoid_step_x = -avoid_step_x
+	end
+	local avoid_step_z = MAPGEN_AVOID_STEPS
+	if (math.random() < 0.5) then
+		avoid_step_z = -avoid_step_z
+	end
+
 	-- if too close to an avoid origin, move the location until we're far enough
 	for i, org in ipairs(avoid) do
 		local dist = calculate_distance(pos, org)
 		while (dist.x < distance) or (dist.z < distance) do
 			if (dist.x < distance) then
-				pos.x = pos.x + MAPGEN_AVOID_STEPS
-				dist.x = dist.x + MAPGEN_AVOID_STEPS
+				pos.x = pos.x + avoid_step_x
 			end
 			if (dist.z < distance) then
-				pos.z = pos.z + MAPGEN_AVOID_STEPS
-				dist.z = dist.z + MAPGEN_AVOID_STEPS
+				pos.z = pos.z + avoid_step_z
 			end
 
 			-- check that the origin is still in bounds, and fail the attempt if not
@@ -97,6 +105,8 @@ local function spawn_structure (filename, pos, size, distance, node, range, avoi
 			(pos.z < pos.z - range / 2) or (pos.z > pos.z + range / 2) then
 				return nil
 			end
+
+			dist = calculate_distance(pos, org)
 		end
 	end
 
@@ -104,6 +114,11 @@ local function spawn_structure (filename, pos, size, distance, node, range, avoi
 	-- if we don't, this attempt to spawn the structure is lost
 	local search_target = pos.y - range
 	for search = pos.y, search_target, -1 do
+		-- check if the position is within the structure's height range
+		if (search < height.minimum) or (search > height.maximum) then
+			return nil
+		end
+
 		local pos_here = { x = pos.x, y = search, z = pos.z }
 		local node_here = minetest.env:get_node(pos_here)
 		local pos_down = { x = pos.x, y = search - 1, z = pos.z }
@@ -113,34 +128,32 @@ local function spawn_structure (filename, pos, size, distance, node, range, avoi
 			-- create our structure
 			local pos1 = { x = pos_here.x - size.x / 2, y = pos_here.y, z = pos_here.z - size.z / 2 }
 			local pos2 = { x = pos_here.x + size.x / 2, y = pos_here.y + size.y, z = pos_here.z + size.z / 2 }
-			io_area_import(pos1, pos2, 0 , filename)
+			io_area_import(pos1, pos2, angle, filename)
 
 			-- we spawned the structure on a suitable node, but what if it was the top of a peak?
 			-- to avoid parts of the building left floating, cover everything until all 4 corners touch the ground
-			if (MAPGEN_FILL == true) then
-				for cover_y = pos_down.y, search_target, -1 do
-					local c1 = { x = pos1.x, y = cover_y, z = pos1.z }
-					local c2 = { x = pos1.x, y = cover_y, z = pos2.z }
-					local c3 = { x = pos2.x, y = cover_y, z = pos1.z }
-					local c4 = { x = pos2.x, y = cover_y, z = pos2.z }
-					local n1 = minetest.env:get_node(c1)
-					local n2 = minetest.env:get_node(c2)
-					local n3 = minetest.env:get_node(c3)
-					local n4 = minetest.env:get_node(c4)
+			for cover_y = pos_down.y, pos_down.y - MAPGEN_FILL, -1 do
+				local c1 = { x = pos1.x, y = cover_y, z = pos1.z }
+				local c2 = { x = pos1.x, y = cover_y, z = pos2.z }
+				local c3 = { x = pos2.x, y = cover_y, z = pos1.z }
+				local c4 = { x = pos2.x, y = cover_y, z = pos2.z }
+				local n1 = minetest.env:get_node(c1)
+				local n2 = minetest.env:get_node(c2)
+				local n3 = minetest.env:get_node(c3)
+				local n4 = minetest.env:get_node(c4)
 
-					if (n1.name == "air") or (n2.name == "air") or (n3.name == "air") or (n4.name == "air") then
-						for cover_x = pos1.x, pos2.x do
-							for cover_z = pos1.z, pos2.z do
-								pos_fill = { x = cover_x, y = cover_y, z = cover_z }
-								node_fill = minetest.env:get_node(pos_fill)
-								if (node_fill.name ~= node) then
-									minetest.env:add_node(pos_fill, { name = node })
-								end
+				if (n1.name == "air") or (n2.name == "air") or (n3.name == "air") or (n4.name == "air") then
+					for cover_x = pos1.x, pos2.x do
+						for cover_z = pos1.z, pos2.z do
+							pos_fill = { x = cover_x, y = cover_y, z = cover_z }
+							node_fill = minetest.env:get_node(pos_fill)
+							if (node_fill.name ~= node) then
+								minetest.env:add_node(pos_fill, { name = node })
 							end
 						end
-					else
-						break
 					end
+				else
+					break
 				end
 			end
 
@@ -170,20 +183,28 @@ local function spawn_group (minp, maxp)
 	-- go through all entries in the mapgen table which belong to this group
 	for i, entry in ipairs(mapgen_table) do
 		if (entry[5] == mapgen_groups[group]) then
-			local size = { x = entry[1], y = entry[2], z = entry[3] }
 			local probability = tonumber(entry[7])
+			local height = { minimum = tonumber(entry[8]), maximum = tonumber(entry[9]) }
 			local distance = tonumber(entry[10])
-
 			-- decide on the distance this building can spawn in
 			local range = (probability + distance) * MAPGEN_RANGE
 
 			for x = 1, probability do
+				-- choose a random angle (0, 90, 180, 270) and adjust the size to that
+				local size = { }
+				local angle = 90 * math.random(0, 3)
+				if (angle == 90) or (angle == 270) then
+					size = { x = entry[3], y = entry[2], z = entry[1] }
+				else
+					size = { x = entry[1], y = entry[2], z = entry[3] }
+				end
+
 				-- randomize X and Z coordinates within range
 				coords_x = pos.x + math.random(-range / 2, range / 2)
 				coords_z = pos.z + math.random(-range / 2, range / 2)
 				local coords = { x = coords_x, y = pos.y, z = coords_z }
 
-				local org = spawn_structure(entry[4], coords, size, distance, entry[6], range, avoid)
+				local org = spawn_structure(entry[4], coords, angle, size, distance, range, height, entry[6], avoid)
 				if (org ~= nil) then
 					table.insert(avoid, org)
 				end
